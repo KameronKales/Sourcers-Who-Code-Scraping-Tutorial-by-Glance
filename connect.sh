e8896dd0dff30200b4d1bed9679c7f1cce18c93f
Kam <kam@glance.ai>
2017-08-24 00:59:53 -0400

    Adding config files to run on AWS

---
+++ connect.sh
@@ -0,0 +1,1 @@
+ssh -i "web_scraper.pem" ubuntu@ec2-34-228-162-134.compute-1.amazonaws.com
---
+++ update_machine.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y 
+sudo apt-get install python -y 
+sudo apt install python-pip -y 
+git pull https://github.com/KameronKales/web_scraper_example.git
+cd web_scraper_example 
+
+
