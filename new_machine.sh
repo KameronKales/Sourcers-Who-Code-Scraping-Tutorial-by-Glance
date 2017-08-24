404448b22f825b989c03505523793baa9873e669
Kam <kam@glance.ai>
2017-08-24 18:04:59 -0400

    Adding install for git.

--- new_machine.sh
+++ new_machine.sh
@@ -1,6 +1,7 @@
 sudo apt-get update -y 
 sudo apt-get install python -y 
 sudo apt install python-pip -y 
+sudo apt-get install git -y
 git pull https://github.com/KameronKales/web_scraper_example.git
 pip install -r requirements.txt
 python scraper.py
